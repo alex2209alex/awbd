9897893c209bfe11a4271464f1de8965f2e5b9be
Alexandru Pavel <alex.pavel2002@gmail.com>
2025-03-23 17:51:50 +0200

    Initial commit be

---
+++ awbd-be/src/main/java/ro/unibuc/fmi/awbd/AwbdApplication.java
@@ -0,0 +1,13 @@
+package ro.unibuc.fmi.awbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwbdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwbdApplication.class, args);
+	}
+
+}
---
+++ awbd-be/src/test/java/ro/unibuc/fmi/awbd/AwbdApplicationTests.java
@@ -0,0 +1,11 @@
+package ro.unibuc.fmi.awbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwbdApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
