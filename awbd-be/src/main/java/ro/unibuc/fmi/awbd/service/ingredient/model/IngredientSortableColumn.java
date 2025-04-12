91310b092f4371dbd63e17f3adcfb04baa307dd5
Alexandru Pavel <alex.pavel2002@gmail.com>
2025-04-12 22:14:43 +0300

    IngredientSortableColumn + tests

---
+++ awbd-be/src/main/java/ro/unibuc/fmi/awbd/service/ingredient/model/IngredientSortableColumn.java
@@ -0,0 +1,37 @@
+package ro.unibuc.fmi.awbd.service.ingredient.model;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.val;
+import ro.unibuc.fmi.awbd.common.utils.ErrorMessageUtils;
+
+@Getter
+@RequiredArgsConstructor
+public enum IngredientSortableColumn {
+    NAME("name"),
+    PRICE("price"),
+    CALORIES("calories"),
+    PRODUCER("producer");
+
+    private final String columnName;
+
+    public static boolean isValidSortableColumn(String sortableColumn) {
+        for (val column : IngredientSortableColumn.values()) {
+            if (column.name().equalsIgnoreCase(sortableColumn)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static IngredientSortableColumn fromValue(String value) {
+        for (val column : IngredientSortableColumn.values()) {
+            if (column.name().equalsIgnoreCase(value)) {
+                return column;
+            }
+        }
+
+        throw new IllegalArgumentException(String.format(ErrorMessageUtils.INVALID_SORTABLE_COLUMN, "IngredientSortableColumn", value));
+    }
+}
