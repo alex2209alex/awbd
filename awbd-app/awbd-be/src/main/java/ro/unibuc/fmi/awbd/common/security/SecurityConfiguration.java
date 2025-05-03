606568cb388e30c930dc57551808fd58f3e8ffb9
Alexandru Pavel <alex.pavel2002@gmail.com>
2025-05-03 19:29:58 +0300

    Security

--- awbd-app/awbd-be/src/main/java/ro/unibuc/fmi/awbd/common/security/SecurityConfiguration.java
+++ awbd-app/awbd-be/src/main/java/ro/unibuc/fmi/awbd/common/security/SecurityConfiguration.java
@@ -3,7 +3,6 @@ package ro.unibuc.fmi.awbd.common.security;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -27,7 +26,6 @@ public class SecurityConfiguration {
         http
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
-                .httpBasic(Customizer.withDefaults())
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(
                                 "/clients",
